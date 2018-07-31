f43c7de49f76e8cbd6bc8d8f19e5edf44fdb9025
AlexWetzel <wetzela@live.com>
2018-07-30 22:59:23 -0400

    Added topic buttons

--- gif-machine/src/app/app.component.ts
+++ gif-machine/src/app/app.component.ts
@@ -12,15 +12,18 @@ export class AppComponent {
 
   title = 'app';
   gifs: object[] = [];
+  topics: string[] = ["cat", "dog", "ferret", "raccoon", "sloth"];
 
-  apiCall() {
+
+  getTopicFromForm() {
+    const topic = this.queryElement.nativeElement.value;
+    this.apiCall(topic);
+    this.topics.push(topic);
+  }
+
+  apiCall(topic) {
     const searchResults = []
-    console.log('This works');
-
-    // const search = 'cats';
-    const search = this.queryElement.nativeElement.value;
-    console.log(search);
-    const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cb04bd6c938f44708d8dd7cbbefb25ad&q=" + search + "&limit=10&offset=0&lang=en";
+    const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cb04bd6c938f44708d8dd7cbbefb25ad&q=" + topic + "&limit=10&offset=0&lang=en";
 
     axios.get(queryURL).then(function(response){
 
@@ -35,33 +38,6 @@ export class AppComponent {
         console.log(gif);
         searchResults.push(gif);
       });
-
-      // console.log(this.gifs);
-
-      // const result = response.data
-      // console.log(result[0].images.fixed_height.url);
-  
-      // this.gifs = response.data
-      // console.log(this.gifs);
-
-      //This will display 10 gifs with their ratings
-      // for (var i = 0; i < 10; i++) {
-
-      //   var animate = result[i].images.fixed_height.url;
-      //   var still = result[i].images.fixed_height_still.url;
-      //   var rating = result[i].rating;
-
-      //   var gifDiv = $("<div>").addClass("gif-div");
-      //   var gifImg = $("<img>");
-      //   var p = $("<p>");
-
-      //   p.html("rating: " + rating);
-      //   gifImg.attr("src", still).attr("still", still).attr("animate", animate).attr("state", "still");
-
-      //   gifDiv.append(gifImg).append(p);
-      //   $("#gifs").prepend(gifDiv);
-      // }
-      console.log(searchResults);
     });
     this.gifs = searchResults;
   }
