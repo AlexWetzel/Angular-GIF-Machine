37c6c0f39a51dc6ef132880c5faaa0a63672871e
AlexWetzel <wetzela@live.com>
2018-08-01 17:16:34 -0400

    fixed a typo in the gif panel ts

--- gif-machine/src/app/gif-panel/gif-panel.component.ts
+++ gif-machine/src/app/gif-panel/gif-panel.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class GifPanelComponent implements OnInit {
   @Input() gifPanel;
-  isclicked: boolean = false;
+  isClicked: boolean = false;
  
 
   constructor() { }
